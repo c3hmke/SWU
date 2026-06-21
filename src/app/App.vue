80f265bc713a298049a76bd7208c0749f34f3a34
c3hmke <cehmke91@gmail.com>
2026-06-22 09:50:30 +1200

    Some styling tweaks

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="app-shell" spellcheck="false">
     <header class="site-header">
-      <RouterLink to="/cards" class="brand">SWU Singles NZ</RouterLink>
+      <RouterLink to="/cards" class="brand" aria-label="Star Wars: Unlimited Singles NZ">
+        <span class="brand-primary">Star Wars: Unlimited</span>
+        <span class="brand-secondary">Singles NZ</span>
+      </RouterLink>
       <nav class="site-nav" aria-label="Primary navigation">
         <RouterLink to="/cards">Cards</RouterLink>
         <RouterLink to="/bulk-search">Bulk lookup</RouterLink>
